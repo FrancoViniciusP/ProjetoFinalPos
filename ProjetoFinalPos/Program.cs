59fde6120701a7a4c479cc0cd200e533f95ee7a7
Vinicius Pacheco Franco <vpacheco0427@gmail.com>
2025-02-03 20:03:28 -0300

    feat: add swagger doc and nugets

--- ProjetoFinalPos/Program.cs
+++ ProjetoFinalPos/Program.cs
@@ -1,7 +1,25 @@
+using ProjetoFinalPos.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite("Data Source=app.db"));
+
+// Adicione o Swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "My API",
+        Description = "API Documentation"
+    });
+});
 
 var app = builder.Build();
 
@@ -9,7 +27,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -18,12 +35,19 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+// Adicione o middleware do Swagger
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = string.Empty; // Para acessar o Swagger na raiz (opcional)
+});
+
 app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
